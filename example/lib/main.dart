7432c806a95ea1c3a78fb7e26cc8aacec1e1b3c2
Sydney Agcaoili <sydneyagcaoili@gmail.com>
2023-09-22 18:38:56 +0100

    Add example project

---
+++ example/lib/main.dart
@@ -0,0 +1,30 @@
+import 'package:flutter/material.dart';
+import 'package:milkdromeda_icons/milkdromeda_icons.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+        useMaterial3: true,
+      ),
+      home: Scaffold(
+        appBar: AppBar(
+          title: const Text('Milkdromeda Icons Demo'),
+        ),
+        body: const Center(
+          child: Icon(MilkdromedaIcons.chatNow1),
+        ),
+      ),
+    );
+  }
+}
